f9ea21c29adf08dca76ee0eef5615ff4c8fcc788
heitordeve <heitor.developer@outlook.com>
2025-05-22 12:25:21 -0300

    Config - Criação do projeto

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import { Header} from '../components/header';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`antialiased`}
+      >
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
